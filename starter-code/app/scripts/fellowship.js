a6fea5a6dca0f06aea92f83d1a948ceb3e0e4a1f
hockey2249 <kgibbons14@gmail.com>
2016-06-20 07:18:03 -0600

    dom GOT HW

--- starter-code/app/scripts/fellowship.js
+++ starter-code/app/scripts/fellowship.js
@@ -41,59 +41,101 @@ makeMiddleEarth();
 
 // Part 2
 
-// function makeHobbits() {
 
-// //     var shire_hobbits = document.createElement('hobbits');
-// //     shire_hobbits.setAttribute('id', 'shire');
-// //     for( i=0; i < hobbits.length; i++)
-    
+function makeHobbits() {
+
 // //   // display an unordered list of hobbits in the shire (which is the first article tag on the page)
 // //   // give each hobbit a class of hobbit
-// // }
+// // 
+      var hobbitList = document.createElement('ul');
+      hobbitList.setAttribute('class', 'hobbit');
+
+      for (var i = 0; i < hobbits.length; i++) {
+        var hobbit = document.createElement('li');
+        hobbit.textContent = hobbits[i];
+        hobbitList.appendChild(hobbit);
+      }
+      shire.appendChild(hobbitList);
+
+}
+      var shire = document.querySelector('article');
+
+makeHobbits();
 
 
-// // Part 3
+// Part 3
 
 function keepItSecretKeepItSafe() {
   // create a div with an id of 'the-ring'
   // give the div a class of 'magic-imbued-jewelry'
   // add an event listener so that when a user clicks on the ring, the nazgulScreech function (provided) is invoked
   // add the ring as a child of Frodo
+
+  var makeSound = document.createElement('div');
+  makeSound.setAttribute('id', 'the-ring');
+  makeSound.setAttribute('class', 'magic-imbued-jewelry');
+  makeSound.addEventListener('click', nazgulScreech);
+  document.querySelector('li').appendChild(makeSound);
+
 }
+keepItSecretKeepItSafe();
 
 
 // Part 4
 
 
+var lands = document.querySelectorAll('article')[1];
+
 function makeBuddies() {
   // create an aside tag
   // attach an unordered list of the 'buddies' in the aside
   // insert your aside as a child element of rivendell
+
+  var asideMakeBuddies = document.createElement('aside');
+  var buddyList = document.createElement('ul');
+        for(var i = 0; i < buddies.length; i++){
+            var eachBuddy = document.createElement('li');
+            eachBuddy.textContent = buddies[i];
+            buddyList.appendChild(eachBuddy);
+        } 
+        asideMakeBuddies.appendChild(buddyList);
+        lands.appendChild(asideMakeBuddies);
 }
-
-
+ makeBuddies();   
 // Part 5
 
+var strider = lands.querySelectorAll('li')[3];
 
 function beautifulStranger() {
   // change the 'Strider' textnode to 'Aragorn'
+      strider.textContent= 'Aragorn';
 }
 
+beautifulStranger();
 
 // Part 6
 
+var hobbits = shire.querySelector('ul');
+
 function leaveTheShire() {
   // assemble the hobbits and move them to Rivendell
+    lands.appendChild(hobbits);
 }
+
+leaveTheShire();
 
 
 // Part 7
 
+var fellowshipMembers = lands.querySelectorAll('li');
 
 function forgeTheFellowShip() {
   // create a new div called 'the-fellowship' within rivendell
   // add each hobbit and buddy one at a time to 'the-fellowship'
   // after each character is added make an alert that they have joined your party
+  var newDiv = document.createElement('div');
+  newDiv.setAttribute('id', 'the-fellowship');
+
 }
 
 
